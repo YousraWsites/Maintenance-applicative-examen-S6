f7724aa9226a529f12c3c7123868d26d1de64262
Imane Benazzouz <imanebenazzouz@MacBook-Pro-de-Imane.local>
2026-06-25 14:18:27 +0200

    bugfix: vérification montant nul ou négatif

--- app.py
+++ app.py
@@ -12,6 +12,18 @@ amount = st.number_input("Montant :", min_value=0.0, format="%.2f")
 from_currency = st.selectbox("De :", rates.keys())
 to_currency = st.selectbox("Vers :", rates.keys())
 
-if st.button("Convertir"):
-    result = amount * rates[to_currency] / rates[from_currency]
-    st.success(f"{amount} {from_currency} = {result:.2f} {to_currency}")
+
+#AVANT
+#if st.button("Convertir"):
+    #result = amount * rates[to_currency] / rates[from_currency]
+    #st.success(f"{amount} {from_currency} = {result:.2f} {to_currency}")
+
+
+if st.button("Convertir"): #il s'execute quand user cliquise qur bouton
+      if amount <= 0:      # on verifie que amount ne sois pas inferireur ou = à zero     
+          st.error("Le montant doit être supérieur à zéro.")  #afficher message d'erreur rouge (st.error), STOP
+      else:                                    # sinon
+          result = amount * rates[to_currency] / rates[from_currency] #on ramene en euro ( devise de reference) puis on convertis en devise cible 
+          st.success(f"{amount} {from_currency} = {result:.2f} {to_currency}") #on affihce le resultat 
+
+
