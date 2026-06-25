950e4779491b443ec99f0c5ace5c9d4a1c101afa
Imane Benazzouz <imanebenazzouz@MacBook-Pro-de-Imane.local>
2026-06-25 14:20:54 +0200

    bugfix: vérification devises source et cible identiques

--- app.py
+++ app.py
@@ -22,6 +22,11 @@ to_currency = st.selectbox("Vers :", rates.keys())
 if st.button("Convertir"): #il s'execute quand user cliquise qur bouton
       if amount <= 0:      # on verifie que amount ne sois pas inferireur ou = à zero     
           st.error("Le montant doit être supérieur à zéro.")  #afficher message d'erreur rouge (st.error), STOP
+      
+      elif from_currency == to_currency:                        # si devis a = devise b 
+          st.error("Les deux devises ne peuvent pas être identiques.")  # affihcer message erreur 
+           
+  
       else:                                    # sinon
           result = amount * rates[to_currency] / rates[from_currency] #on ramene en euro ( devise de reference) puis on convertis en devise cible 
           st.success(f"{amount} {from_currency} = {result:.2f} {to_currency}") #on affihce le resultat 
